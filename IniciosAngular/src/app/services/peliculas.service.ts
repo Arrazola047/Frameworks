d853914019d59fb4c81943c4c7846f9b063b22ad
Ramon Arrazola <arrazola047@gmail.com>
2023-10-05 18:40:29 -0600

    Se creo un servicio

---
+++ IniciosAngular/src/app/services/peliculas.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+import { Pelicula  } from '../models/pelicula';
+
+@Injectable()
+export class PeliculaService{
+    holaMundo(){
+        return 'Hola Mundo desde un servicio de Angular';
+    }
+}
